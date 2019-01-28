c92c1b28d0054d5db0a8e556dd9aa4cc551330d6
Rob Bade <rob.bade@politie.nl>
2019-01-28 07:46:04 +0100

    Add project files.

---
+++ Motor_NTP_v2/Motor_NTP_v2.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Motor_NTP_v2.ino
+ Created:	1/28/2019 7:45:59 AM
+ Author:	rob_b
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
